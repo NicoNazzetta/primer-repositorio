578026f7eceab887a88536ea6d3f1213802a4399
NicoNazzetta <nico_nazzetta@hotmail.com>
2019-11-25 20:45:31 -0300

    ejercicios html + css

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
-<?php
-
-echo ("bienvenidos a mi primer repositorio");
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Título de la página</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="http://localhost/primer-repositorio/css/pagina.css">
+</head>
+<body class="fondo">
+	<div class="menu">
+		<h2>
+			<a href="http://localhost/primer-repositorio/">Inicio</a>
+		</h2>
+		<ul>
+			<li>
+				<a href="http://localhost/primer-repositorio/about.php">¿Quienes Somos?</a>
+			</li>
+			<li>
+				<a href="http://localhost/primer-repositorio/catalogo.php">Catalogo</a>
+			</li>
+			<li>
+				<a href="http://localhost/primer-repositorio/contactos.php">Contacto</a>
+			</li>
+			<li>
+				<a href="http://localhost/primer-repositorio/clientes.php">Nuestros Clientes</a>
+			</li>
+		</ul>
+	</div>
+	
+</body>
+</html>
